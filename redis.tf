1826bfa693adf5599d956fa0e2b80c6554b1397f
ShehabEl-DeenAlalkamy <essamshehab33_3@h-eng.helwan.edu.eg>
2021-04-07 06:36:19 +0200

    editing

--- redis.tf
+++ redis.tf
@@ -1,11 +1,11 @@
 resource "aws_elasticache_cluster" "redis" {
-  cluster_id           = "cluster-redis"
+  cluster_id           = var.redis_group_id
   engine               = "redis"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
   parameter_group_name = "default.redis3.2"
   engine_version       = "3.2.10"
-  port                 = 6379
+  port                 = var.redis_port
 
 
   subnet_group_name  = aws_elasticache_subnet_group.iti.name
